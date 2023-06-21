3b4e10c6661627b4371695b698178f3ba308e65e
sandajama <sandajama29@gmail.com>
2023-06-21 16:00:32 +0300

    aswered

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Good Luck 💪🏾
+ const reverseWords = (sentence) => {
+    const words = sentence.split(" ")
+    const revwords = words.reverse()
+    return revwords.join(" ")
+}
+
+
+console.log(reverseWords("fikrcamp bootcamp"))// ➞ "bootcamp fikrcamp"
+console.log(reverseWords("hello there!")) //➞ "there! hello"
+console.log(reverseWords("welcome to my city")) //➞ "city my to welcome"
